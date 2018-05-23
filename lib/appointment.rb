8ac35a77b3df1f26f57000289ca97b3fd135e2e7
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-23 06:00:44 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,10 @@
 class Appointment
 
-  attr_accessor :date
+  attr_accessor :name, :date
   @@all = []
 
-  def initialize(date)
+  def initialize(name, date)
+    @name = name
     @date = date
     @@all << self
   end
