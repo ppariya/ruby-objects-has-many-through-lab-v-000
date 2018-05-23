f1ddf5fd956ed0cddb16b95e91456417f3e84bad
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-23 05:33:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
   end
 
   def new_song(title, genre)
-    song = Song.new(title, name, genre)
+    song = Song.new(title, self, genre)
     self.songs << song
     song.artist = self
   end
